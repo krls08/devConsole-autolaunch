d7d22b9737bd30a1c2d20097c2152125a25fabc6
krls08 <cpujades@albirasolutions.com>
2020-07-20 22:13:18 +0200

    upd

--- src/autoLaunchAnonymous.js
+++ src/autoLaunchAnonymous.js
@@ -1,34 +1,55 @@
-// Assing button to click element
-const exec = document.querySelector('#button-1188-btnEl');
-
-// create keyboard event ctrl + e
-const kbEv = new KeyboardEvent('keydown',{bubbles:true, ctrlKey:true, keyCode:69});
-
-// create a sleep function
-const sleep = (ms) => {
-    return new Promise(resolve => setTimeout(resolve,ms));
-}
+'use strict';
 
 class clickEv{
-    constructor(button,ev){
-        this.btn = button;
-        this.kbEv = ev;
+    constructor(){
+        this.btn;
+        this.kbEv = new KeyboardEvent('keydown',{bubbles:true,
+                                                    ctrlKey:true,
+                                                    keyCode:69});
+        this.setup();
+
     }
-    // sleep method
-    sl = (ms) => {
+    // setup
+    setup = async () =>{
+        this._openAnon();
+        await this._sl(3000);
+        this._closeAnon();
+        this.btn = document.querySelector('#button-1188-btnEl');
+    }
+    _openAnon = () =>{
+        document.body.dispatchEvent(this.kbEv);
+    }
+    _closeAnon = () =>{
+        const close = document.querySelector('#tool-1820-toolEl');
+        close.click();
+    }
+    // sleep method -> Run in Async!
+    _sl = (ms) => {
         return new Promise(resolve => setTimeout(resolve,ms));
     }
-    // launch the main function
-    start = async (iterations, toShowScript = 4000, timeAfterClick = 50000) => {
+    // launch the main method
+    start = async (iterations, timeAfterClick = 50000, toShowScript = 4000) => {
         for(let i = 0; i < iterations; i++){
             document.body.dispatchEvent(this.kbEv);
-            await this.sl(toShowScript);
+            await this._sl(toShowScript);
             this.btn.click();
-            await this.sl(timeAfterClick);
+            await this._sl(timeAfterClick);
+            console.log('iteration: ', i);
+        }
+    }
+    // silent method -> do not show the scripting window
+    silentLaunch = async (iterations, timeAfterClick = 50000) => {
+        for(let i = 0; i < iterations; i++){
+            this.btn.click();
+            await this._sl(timeAfterClick);
             console.log('iteration: ', i);
         }
     }
 }
-anonFunc = new clickEv(exec,kbEv);
 
-anonFunc.start(3);
+let anonFunc = new clickEv();
+
+//anonFunc.start(3,5000,3000);
+//anonFunc.start(3,4000);
+//anonFunc.silentLaunch(3,6000);
+
